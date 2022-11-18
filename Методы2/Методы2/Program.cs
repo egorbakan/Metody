366370f28cf44782b5bf83a4f4e23c813b153efe
egorbakan <118420911+egorbakan@users.noreply.github.com>
2022-11-18 15:25:55 +0900

    Add files via upload

---
+++ Методы2/Методы2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Методы2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, sum;
+
+            Console.WriteLine("Введите первое число");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            sum = a + b;
+            Console.WriteLine("Ответ: " + sum);
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            float c, d, multiply;
+
+            Console.WriteLine("Введите первое число");
+            c = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            d = Convert.ToInt32(Console.ReadLine());
+
+            multiply = c * d;
+            Console.WriteLine("Ответ: " + multiply);
+        }
+
+        public void Sum(float sum)
+        {
+            Console.WriteLine(+sum);
+        }
+
+        private void Mult(float multiply)
+        {
+            Console.WriteLine(+multiply);
+        }
+
+
+    }
+}
---
+++ Методы2/Методы2/Users.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Методы2
+{
+    class Users
+    {
+    }
+}
